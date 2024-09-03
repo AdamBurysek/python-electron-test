c592d260edb49a52f8eca487a5cae55cf000eaee
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-09-03 16:42:17 +0200

    Add cn function

--- src/renderer/src/components/Button.tsx
+++ src/renderer/src/components/Button.tsx
@@ -1,13 +1,19 @@
+import { cn } from '@renderer/lib/utils'
+
 interface Props {
   onClick: () => void
   label: string
+  className?: string
 }
 
-const Button = ({ onClick, label }: Props): JSX.Element => {
+const Button = ({ onClick, label, className }: Props): JSX.Element => {
   return (
     <button
       onClick={onClick}
-      className="bg-cyan-400 rounded-xl w-40 py-2 text-black font-semibold hover:bg-cyan-300"
+      className={cn(
+        'bg-cyan-400 rounded-xl w-40 py-2 text-black font-semibold hover:bg-cyan-300',
+        className
+      )}
     >
       {label}
     </button>
