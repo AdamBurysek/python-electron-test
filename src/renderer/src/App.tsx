0051114c81cea69dfd077544761c79e9100e59f2
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-09-03 16:34:37 +0200

    Add some styling

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -43,20 +43,21 @@ function App(): JSX.Element {
       window.api.send('stop-python-script')
     }
   }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-3xl font-bold">Ovládání Python Skriptu</h1>
+    <div className="flex flex-col items-center justify-center h-screen gap-4 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+      <h1 className="text-3xl font-bold text-white pb-8">Ovládání Python Skriptu</h1>
       <div className="flex gap-4">
         <Button
           onClick={isRunning ? handleStopScript : handleRunScript}
           label={isRunning ? 'Zastavit skript' : 'Spustit skript'}
         />
       </div>
-      <p className="pt-4 text-xl font-semibold">{output}</p>
+      <p className="pt-4 text-xl font-semibold text-white">{output}</p>
       {error && (
         <>
-          <h2>Chyby</h2>
-          <p>{error}</p>
+          <h2 className="text-xl font-semibold text-white">Chyby</h2>
+          <p className="pt-4 text-xl font-semibold text-white">{error}</p>
         </>
       )}
     </div>
--- src/renderer/src/components/Button.tsx
+++ src/renderer/src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button = ({ onClick, label }: Props): JSX.Element => {
   return (
     <button
       onClick={onClick}
-      className="bg-cyan-700 rounded-xl px-4 py-2 text-white font-semibold hover:bg-cyan-600"
+      className="bg-cyan-400 rounded-xl w-40 py-2 text-black font-semibold hover:bg-cyan-300"
     >
       {label}
     </button>
